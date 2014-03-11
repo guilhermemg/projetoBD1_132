5288dbe6758792382123b6b1b40581c67b4829ae
Armstrong <armstrong@loki.(none)>
2014-03-11 15:56:02 -0300

    - Criei um diretório a ser utilizado na etapa 3 do projeto. - Adicionei o script gabarito da etapa 2. - Adicionei o script resposta para as consultas e o script resposta para as inserções.

---
+++ fase_3/insercoes.sql
@@ -0,0 +1,17 @@
+-- Universidade Federal de Campina Grande
+-- Centro de Engenharia Eletrica e Informatica
+-- Departamento de Sistemas e Computacao
+-- Curso de Ciência da Computacao
+
+-- Disciplina: Banco de Dados 1
+-- Professor: Claudio Baptista
+
+-- Projeto Etapa 3
+-- Equipe: Anderson Thelles Claudino da Silva
+--         Armstrong Mardilson da Silva Goes 
+--         Guilheme Monteiro Gadelha
+--         Werton Guimarães Gomes 
+ 
+
+-- Script de resposta (Insercoes)
+
---
+++ fase_3/respostas.sql
@@ -0,0 +1,49 @@
+-- Universidade Federal de Campina Grande
+-- Centro de Engenharia Eletrica e Informatica
+-- Departamento de Sistemas e Computacao
+-- Curso de Ciência da Computacao
+
+-- Disciplina: Banco de Dados 1
+-- Professor: Claudio Baptista
+
+-- Projeto Etapa 3
+-- Equipe: Anderson Thelles Claudino da Silva
+--         Armstrong Mardilson da Silva Goes 
+--         Guilheme Monteiro Gadelha
+--         Werton Guimarães Gomes 
+ 
+
+-- Script de resposta (Consultas)
+
+
+-- 1. Liste todos os arbitros cadastrados.
+
+-- 2. Qual o arbitro com a menor matricula?
+
+-- 3. Crie uma view que liste o nome dos times por ordem alfabetica.
+
+-- 4. Faca uma trigger que obrigue que um jogo tenha somente jogadores dos dois times. 
+
+-- 5. Faca uma trigger que obrigue que todos os profissionais sejam maiores de 18 anos considerando para este calculo a data de insercao do profissional.
+
+-- 6. Liste todos os estadios ordenados, decrescentemente, pela capacidade de publico.
+
+-- 7. Qual a media de capacidade dos estadios cadastrados?
+
+-- 8. Liste somente os jogadores que um time especifico possui passe no momento da consulta.
+
+-- 9. Liste apenas os estadios cujo nome contenha as letras "P" ou "B".
+
+-- 10. Liste apenas os jogos que serao encerrados entre 17h e 21h (considere que os jogos têm duração exata de 90 minutos).
+
+-- 11. Liste o nome do jogador e o total de passes que ele ja teve com os times ao longo de sua carreira, por ordem decrescente do total de passes. 
+-- So devem ser incluidos na lista os jogadores que tenham registrado passe com, pelo menos, dois times.
+
+-- 12. Quais foram os jogadores que marcaram gols na rodada do dia 23/02/2014?
+
+-- 13. Liste os jogadores com mais de 2 cartoes de vermelhos.
+
+-- 14. Crie uma view que liste os times que possuem contrato de 2 anos ou mais com pelo menos 1 jogador.
+
+-- 15. Quais foram os tecnicos que treinaram mais de um time no campeonato?
+
